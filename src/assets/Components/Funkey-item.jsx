96a946ff538af0030e447d0676e52c9e762d3fd9
Coolteees-2025 <coolteees2025@gmail.com>
2025-04-17 23:34:51 +0530

    Update package.json and vite.config.js

--- src/assets/Components/Funkey-item.jsx
+++ src/assets/Components/Funkey-item.jsx
@@ -34,6 +34,17 @@ const FunkeyItem = [
       originalPrice: 1999,
       discount: 25,
       fabric: "THICK PREMIUM FABRIC"
+    },{
+      link: "/p/mens-green-sasuke-graphic-printed-oversized-t-shirt-forest-biome",
+      image: "https://images.bewakoof.com/t320/men-s-green-sasuke-graphic-printed-oversized-t-shirt-659575-1743679494-1.jpg",
+      title: "Men's Green Sasuke Graphic Printed Oversized T-shirt",
+      rating: "4.5",
+      isNewArrival: true,
+      brand: "bewakoof x naruto",
+      price: 1299,
+      originalPrice: 1999,
+      discount: 25,
+      fabric: "THICK PREMIUM FABRIC"
     }
   ];
   export default FunkeyItem;
